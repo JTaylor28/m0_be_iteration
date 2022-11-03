fd68c4ae3802738f1469d09645cc8a83215b8dd0
James Taylor <jrtaylor0614@gmail.com>
2022-11-02 21:15:59 -0600

    Fix challenge 2

--- each.rb
+++ each.rb
@@ -11,12 +11,12 @@ weather_options.each do |weather|
 end
 
 
-# Challenge 2
-# Use the each method to print out the words that are exactly 3 characters in length
+Challenge 2
+Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 
 words.each do |word|
-  if word.leangth == 3
+  if word.length == 3
     puts word
   end
 end
